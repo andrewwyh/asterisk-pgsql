99e1436a03620fafd5fdea6a9603ef1ceb56f364
andrewwyh <yehim.wong@gmail.com>
2020-06-18 07:57:40 +0800

    Improve top bar nav, context and endpoint pages

--- laravel/application/laravel-7-crud-app/resources/views/base.blade.php
+++ laravel/application/laravel-7-crud-app/resources/views/base.blade.php
@@ -9,23 +9,32 @@
 
 <div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Avenum Cloud PBX</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
+
+      <a style="margin: 19px;" href="{{ route('endpoints.index')}}" class="btn btn-primary">Show All Extensions</a>
+      <form class="form-inline my-2 my-lg-0" method="get" action="/context">
+      @csrf 
+      <input class="form-control mr-sm-2" name="context" type="search" placeholder="Search by Company" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      <a style="margin: 19px;" href="{{ route('endpoints.create')}}" class="btn btn-primary">New contact</a>
+
+<!Nav items below just in case>
+<ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href=""></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href=""></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
+        Disabled</a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
@@ -34,13 +43,10 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link disabled" href="#"></a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+
   </div>
 </nav>
 </div>
